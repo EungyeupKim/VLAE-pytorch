8fb187ae3275a39b8de72a1f97d68a87e03e3e06
EungyeupKim <yhy1254@gmail.com>
2019-10-02 17:24:19 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,155 @@
+import argparse
+import logging
+import sys
+import os
+import torch
+from configparser import ConfigParser
+
+from torch import optim
+
+from disvae.training import Trainer
+from disvae.utils.modelIO import save_model, load_model, load_metadata
+from disvae.models.losses import RECON_DIST
+from disvae.models.vae import VLAE
+from utils.datasets import get_dataloaders, get_img_size, DATASETS
+from utils.helpers import (create_directory, get_device, set_seed, get_n_param,
+                           get_config_section, update_namespace_, FormatterNoDuplicate)
+from utils.visualize import GifTraversalsTraining
+
+RES_DIR = "results"
+#LOG_LEVELS = list(logging._levelToName.values())
+# ADDITIONAL_EXP = ['custom', "debug", "best_celeba", "best_dsprites"]
+# EXPERIMENTS = ADDITIONAL_EXP + ["{}_{}".format(loss, data)
+#                                 for loss in LOSSES
+#                                 for data in DATASETS]
+
+def parse_arguments(args_to_parse):
+    """Parse the command line arguments.
+
+    Parameters
+    ----------
+    args_to_parse: list of str
+        Arguments to parse (splitted on whitespaces).
+    """
+    description = "PyTorch implementation and evaluation of disentangled Variational AutoEncoders and metrics."
+    parser = argparse.ArgumentParser(description=description)
+
+    # General options
+    parser.add_argument('name', type=str,
+                         help="Name of the model for storing and loading purposes.")
+    # general.add_argument('-L', '--log-level', help="Logging levels.",
+    #                      default=default_config['log_level'], choices=LOG_LEVELS)
+    parser.add_argument('--no-progress-bar', action='store_true',
+                         default=False,
+                         help='Disables progress bar.')
+    parser.add_argument('-g', '--gpu', type=int, default=0, help='the number of gpu to use')
+    parser.add_argument('-s', '--seed', type=int, default=1234,
+                         help='Random seed. Can be `None` for stochastic behavior.')
+
+    # Learning options
+    parser.add_argument('--checkpoint-every', type=int, default=30,
+                          help='Save a checkpoint of the trained model every n epoch.')
+    parser.add_argument('-d', '--dataset', help="Path to training data.", default='mnist', choices=DATASETS)
+    parser.add_argument('-e', '--epochs', type=int, default=100, help='Maximum number of epochs to run for.')
+    parser.add_argument('-b', '--batch-size', type=int, default=64, help='Batch size for training.')
+    parser.add_argument('--lr', type=float, default=5e-4, help='Learning rate.')
+
+    # Model Options
+    parser.add_argument('-z', '--latent_dim', type=int, default=2, help='Dimension of the latent variable.')
+    parser.add_argument('-r', '--rec-dist', default='laplace', choices=RECON_DIST,
+                       help="Form of the likelihood ot use for each pixel.")
+    parser.add_argument('-a', '--reg-anneal', type=float, default=10000,
+                       help="Number of annealing steps where gradually adding the regularisation. What is annealed is specific to each loss.")
+
+    # Loss Specific Options
+    parser.add_argument('--reg_coeff0', type=int, default=1.0)
+    parser.add_argument('--reg_coeff1', type=int, default=1.0)
+    parser.add_argument('--reg_coeff2', type=int, default=1.0)
+
+    # Learning options
+    # evaluation = parser.add_argument_group('Evaluation specific options')
+    parser.add_argument('--is-eval-only', action='store_true',
+                            default=False,
+                            help='Whether to only evaluate using precomputed model `name`.')
+    # evaluation.add_argument('--is-metrics', action='store_true',
+    #                         default=default_config['is_metrics'],
+    #                         help="Whether to compute the disentangled metrcics. Currently only possible with `dsprites` as it is the only dataset with known true factors of variations.")
+    # evaluation.add_argument('--no-test', action='store_true',
+    #                         default=default_config['no_test'],
+    #                         help="Whether not to compute the test losses.`")
+    # evaluation.add_argument('--eval-batchsize', type=int,
+    #                         default=default_config['eval_batchsize'],
+    #                         help='Batch size for evaluation.')
+
+    args = parser.parse_args(args_to_parse)
+
+    return args
+
+def main(args):
+    """Main train and evaluation function.
+
+    Parameters
+    ----------
+    args: argparse.Namespace
+        Arguments
+    """
+
+    set_seed(args.seed)
+    device = torch.device('cuda:{}'.format(args.gpu) if torch.cuda.is_available() else 'cpu')
+    exp_dir = os.path.join(RES_DIR, args.name)
+    print("save and load experiments at : {}".format(exp_dir))
+
+    if not args.is_eval_only: #train
+
+        create_directory(exp_dir)
+
+        # PREPARES DATA
+        train_loader = get_dataloaders(args.dataset, batch_size=args.batch_size)
+        # PREPARES MODEL
+        args.img_size = get_img_size(args.dataset)  # stores for metadata
+        cs = [1, 64, 128, 1024]
+        model = VLAE(args.latent_dim, cs)
+
+        # TRAINS
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+        model = model.to(device)  # make sure trainer and viz on same device
+        gif_visualizer = GifTraversalsTraining(model, args.dataset, exp_dir)
+
+        reg_coeff = [args.reg_coeff0, args.reg_coeff1, args.reg_coeff2]
+
+        trainer = Trainer(model, optimizer, reg_coeff,
+                          device=device,
+                          save_dir=exp_dir,
+                          is_progress_bar=not args.no_progress_bar,
+                          gif_visualizer=gif_visualizer)
+
+        trainer(train_loader,
+                epochs=args.epochs,
+                checkpoint_every=args.checkpoint_every)
+
+        #SAVE MODEL AND EXPERIMENT INFORMATION
+
+        save_model(trainer.model, exp_dir, metadata=vars(args))
+        print("Model has been saved")
+    #
+    # if args.is_metrics or not args.no_test:
+    #     model = load_model(exp_dir, is_gpu=not args.no_cuda)
+    #     metadata = load_metadata(exp_dir)
+    #     # TO-DO: currently uses train datatset
+    #     test_loader = get_dataloaders(metadata["dataset"],
+    #                                   batch_size=args.eval_batchsize,
+    #                                   shuffle=False)
+    #                                   # logger=logger)
+    #
+    #     evaluator = Evaluator(model,
+    #                           device=device,
+    #                           # logger=logger,
+    #                           save_dir=exp_dir,
+    #                           is_progress_bar=not args.no_progress_bar)
+    #
+    #     evaluator(test_loader, is_metrics=args.is_metrics, is_losses=not args.no_test)
+
+if __name__ == '__main__':
+    args = parse_arguments(sys.argv[1:])
+    main(args)
